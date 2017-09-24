a58574b86d6bc165e4b1f4baeae2d74b1fc1879c
Peter DeGroff <dagoof@gmail.com>
2017-09-25 01:29:01 +1000

    unification: unify within objects, reuse keys. nearly
    Not quite right still.
    
    Need a smarter Map.merge function to that manages to set keys
    with non-union values to optional decoder.
    
    Need to make sure we aren't shooting ourselves in the foot for future
    recursive detection.
    
    Potentially swap between combine-objects and unify-decoders at an object
    level depending on how similar any given objects in an array [ or
    globally? ] are.
    
    ie. [ obja | objb | objb | obja ]
    
    may be preferable to
    
    { a: optional, b : optional, c : optional, d : optional }
    
    if the objects don't really have much in common. Seems to become a
    search problem.

--- elm.ml
+++ elm.ml
@@ -11,18 +11,63 @@ type basic_decoder =
 
 type composite_decoder =
   [ basic_decoder
+  | `Optional of composite_decoder
   | `Array  of composite_decoder
   | `Object of (string * composite_decoder) list
   | `OneOf  of composite_decoder list
   ] [@@deriving show]
 
+
+let object_part = function
+  | `Object xs -> Some xs
+  | otherwise  -> None
+
+let is_object = function
+  | `Object xs -> true
+  | otherwise  -> false
+
+let objects objs =
+  List.filter_map object_part objs
+
+let non_objects objs =
+  List.filter (is_object >>> not) objs
+
 let unify_decoders decoders =
   match List.unique decoders with
-  | [x] -> `Array x
+  | [x] -> x
   | xs  -> `OneOf xs
 
+let mergesets a b =
+  let emptyset = Opt.default Set.empty in
+  Set.union ( emptyset a ) ( emptyset b )
+
+let combine_objects objs =
+  List.of_enum @@
+  Enum.map begin fun (key, decoders) ->
+    (key, unify_decoders @@ Set.to_list decoders)
+  end @@
+  Map.enum @@
+  List.fold_left begin fun sofar obj ->
+    let next =
+      List.fold_left begin fun map (field, decoder) ->
+        Map.modify_opt field begin fun x ->
+          match x with
+          | Some decoders -> Opt.some @@ Set.add decoder decoders
+          | None ->          Opt.some @@ Set.singleton decoder
+        end map
+      end Map.empty obj
+    in
+
+    Map.merge (fun key a b -> Opt.some @@ mergesets a b ) sofar next
+  end Map.empty objs
+
+
 let rec decode = function
-  | `A vals   -> unify_decoders @@ List.map decode vals
+  | `A vals   ->
+    let decoders = List.map decode vals in
+    let objs = `Object ( combine_objects @@ objects decoders ) in
+    `Array (unify_decoders @@ objs :: non_objects decoders)
+
   | `O vals   -> `Object (List.map (fun (k, v) -> k, decode v) vals)
   | `Null     -> `Null
   | `Bool   v -> `Bool
@@ -101,6 +146,14 @@ let the_json =
      , `A
          [ `O [ "image_url", `String "https://test.com" ]
          ; `O [ "image_url", `String "Up" ]
+         ; `O [ "image_url", `Bool false ]
+         ]
+     ; "data2"
+     , `A
+         [ `O [ "always", `Bool true;   "image_url", `String "https://test.com" ]
+         ; `O [ "always", `Bool false;  "user_profile", `String "tester" ]
+         ; `O [ "always", `String "ok"; "image_url", `String "Up"; "user_profile", `String "sure" ]
+         ; `O [ "always", `Bool true;   "image_url", `Bool false ]
          ]
      ]
 
